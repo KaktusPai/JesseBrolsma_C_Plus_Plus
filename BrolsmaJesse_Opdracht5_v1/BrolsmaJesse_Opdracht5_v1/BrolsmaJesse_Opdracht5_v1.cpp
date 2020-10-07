58e77024c56297de08218430d413caa6f3381f74
KaktusPai <59810116+KaktusPai@users.noreply.github.com>
2020-10-07 17:31:30 +0200

    opdr 5 gestart

---
+++ BrolsmaJesse_Opdracht5_v1/BrolsmaJesse_Opdracht5_v1/BrolsmaJesse_Opdracht5_v1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Bibliotheek {
+    public:
+        void leenUit();
+};
+
+class Boek : public Bibliotheek {
+
+};
+
+void Bibliotheek::leenUit() {
+
+}
+
+int main()
+{
+    
+}
